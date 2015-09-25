9aaffc9304bfa4962ea7d0215bd33a3f95e1a035
Jacob Leonard <jleonrd2@linux-a2.ews.illinois.edu>
2015-09-24 22:09:56 -0500

    name changes

---
+++ node.h
@@ -0,0 +1,14 @@
+#ifndef NODE_H
+#define NODE_H
+
+struct Node
+{
+int x;
+int y;
+Node *left;
+Node *right;
+Node *up;
+Node *down;
+Node *parent;
+};
+#endif
---
+++ tree.h
@@ -0,0 +1,30 @@
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "node.h"
+
+
+
+class Tree
+{
+public:
+
+	Tree(): root(NULL){}
+
+	Tree(int X, int Y);
+
+	~Tree();
+
+	Node* insert(Node *cur, int X, int Y);
+
+	Node* get_root();
+
+private:
+
+	Node *root;
+
+	void delete_node(Node * &me);
+};
+#endif
